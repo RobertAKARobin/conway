8531d2a9a0e1bc1b9e27670a4384d3db4fbd491d
RobertAKARobin <contact@robertakarobin.com>
2021-01-26 11:54:51 -0600

    Prints board

--- index.js
+++ index.js
@@ -1,78 +1,94 @@
-const ROWS = 3;
-const COLS = 3;
-const LIVE = '0';
-const DEAD = '.';
-const NUM_GENERATIONS = 5;
-const STARTING_LIVE_CELLS = [
-  '2,2',
-];
+const Y_MAX = 20;
+const Y_MIN = 1;
+const X_MAX = 40;
+const X_MIN = 1;
+const IS_LIVE = '0';
+const IS_DEAD = '.';
+const GEN_MAX = 3;
 
-let state = boardDraw(ROWS, COLS, STARTING_LIVE_CELLS);
+function assert(value) {
+  if (!value) {
+    throw false;
+  }
+  return value;
+}
 
-for (let generation = 1; generation <= NUM_GENERATIONS; generation++) {
-  console.log(`GENERATION ${generation}`);
-  state = generationNew(state);
-  boardRender(state);
+const Board = {
+  drawNew(xMin, xMax, yMin, yMax) {
+    const board = [];
+    for (let y = yMin; y <= yMax; y++) {
+      const row = [];
+      for (let x = xMin; x <= xMax; x++) {
+        const cell = IS_DEAD;
+        row.push(cell);
+      }
+      board.push(row);
+    }
+    return board;
+  },
+
+  cellGet(board, x, y) {
+    try {
+      return assert(board[y - 1][x - 1]);
+    } catch(e) {
+      console.warn(`Can't get (${x},${y}).`);
+      return null;
+    }
+  },
+
+  cellSet(board, x, y, isLive) {
+    try {
+      board[y - 1][x - 1] = isLive;
+    } catch(e) {
+      console.warn(`Can't set (${x},${y}).`);
+    }
+  },
+
+  render(board) {
+    return board.map(row => row.join('')).join('\n');
+  },
+
+  step(board) {
+    return board;
+  },
+}
+
+let board = Board.drawNew(X_MIN, X_MAX, Y_MIN, Y_MAX);
+Board.cellSet(board, 10, 10, IS_LIVE);
+
+for (let gen = 1; gen <= GEN_MAX; gen++) {
+  console.log(`GENERATION ${gen}`);
+  board = Board.step(board);
+  console.log(Board.render(board));
   console.log('');
 }
 
-function boardDraw(rows, cols, liveCoords = []) {
-  const board = [];
-  for (let rowIndex = 1; rowIndex <= rows; rowIndex++) {
-    const row = [];
-    for (let colIndex = 1; colIndex <= cols; colIndex++) {
-      const coord = `${colIndex},${rowIndex}`;
-      if (liveCoords.includes(coord)) {
-        row.push(LIVE);
-      } else {
-        row.push(DEAD);
-      }
+/*
+1. Any live cell with fewer than two live neighbors dies, as if by under population.
+2. Any live cell with two or three live neighbors lives on to the next generation.
+3. Any live cell with more than three live neighbors dies, as if by overpopulation.
+4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+for each row as row {
+  for each col as col {
+    cell = boardGetAt(board, row, col);
+    if (cell === DEAD) {
+      continue
     }
-    board.push(row);
-  }
-  return board;
-}
-
-function boardGetAt(board, row, col) {
-  
-}
-
-function boardRender(board) {
-  for (const row of board) {
-    console.log(row.join(''));
-  }
-}
-
-function generationNew(board) {
-  const newState = [];
-  /*
-  1. Any live cell with fewer than two live neighbors dies, as if by under population.
-  2. Any live cell with two or three live neighbors lives on to the next generation.
-  3. Any live cell with more than three live neighbors dies, as if by overpopulation.
-  4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-
-  for each row as row {
-    for each col as col {
-      cell = boardGetAt(board, row, col);
-      if (cell === DEAD) {
-        continue
-      }
-      
-      let numberOfLiveNeighbors = 0;
-      const neighborColMin = col - 1;
-      const neighborRowMin = row - 1;
-      const neighborColMax = col + 1;
-      const neighborRowMax = row + 1;
-      for (let neighborRow = neighborRowMin; neighborRow < neighborRowMax; neighborRow++) {
-        for (let neighborCol = neighborColMin; neighborCol < neighborColMax; neighborCol++) {
-          const neighbor = boardGetAt(board, neighborRow, neighborCol);
-          if (neighbor === LIVE) {
-            numberOfLiveNeighbors += 1;
-          }
+    
+    let numberOfLiveNeighbors = 0;
+    const neighborColMin = col - 1;
+    const neighborRowMin = row - 1;
+    const neighborColMax = col + 1;
+    const neighborRowMax = row + 1;
+    for (let neighborRow = neighborRowMin; neighborRow < neighborRowMax; neighborRow++) {
+      for (let neighborCol = neighborColMin; neighborCol < neighborColMax; neighborCol++) {
+        const neighbor = boardGetAt(board, neighborRow, neighborCol);
+        if (neighbor === LIVE) {
+          numberOfLiveNeighbors += 1;
         }
       }
     }
   }
-  */
-  return newstate;
 }
+*/
